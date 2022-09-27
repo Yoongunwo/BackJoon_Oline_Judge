f4fe90fcb7be8a3c31ebe7d866ede288af73d49b
Gunwo Yoon <97718735+Yoongunwo@users.noreply.github.com>
2022-09-27 19:28:40 +0900

    Update 4948_베르트랑공준.java

--- 4948_베르트랑공준.java
+++ 4948_베르트랑공준.java
@@ -1,1 +1,22 @@
 
+import java.util.Scanner;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int n;
+        while((n=scanner.nextInt()) != 0){
+            int sum = 0;
+            int[] arr = new int[2*n+1];
+            for(int i=2; i<arr.length; i++){
+                if(arr[i] == 0){
+                    if(i>n) sum++;
+                    int mul = 2;
+                    for(int j=mul*i; j<arr.length; j=++mul*i) arr[j] = 1;
+                }
+            }
+            System.out.println(sum);
+        }
+        System.out.print(Math.sqrt(13));
+    }
+}
