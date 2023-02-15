84bcd9edc8dc5b8905c6f49d0e67c3a02b1fe960
Gunwo Yoon <97718735+Yoongunwo@users.noreply.github.com>
2023-02-15 13:02:36 +0900

    Update 02110_공유기설치.java

--- 이분 탐색/02110_공유기설치.java
+++ 이분 탐색/02110_공유기설치.java
@@ -1,49 +1,39 @@
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Main {
-    static int numHouse;
-    static int numWifi;
-    static int[] locationHouse;
-    static int[] locationWifi;
-
-    static int findDistance() {
-        int low = 0;
-        int high = locationHouse[locationHouse.length - 1];
-        while (low < high) {
-            int mid = (high + low) /2;
-            if(installWifi(mid) < numWifi){
-                high = mid;
-            }else{
-                low = mid+1;
-            }
-        }
-        return low;
-    }
-
-    static int installWifi(int distance) {
-        int lastLocation = locationHouse[0];
-        int wifi = 1;
-        for (int i = 1; i < numHouse; i++) {
-            if ((locationHouse[i] - lastLocation) >= distance) {
-                wifi++;
-                lastLocation = locationHouse[i];
-            }
-        }
-        return wifi;
-    }
-
-    public static void main(String[] args) {
-
+public class Main{
+    public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        numHouse = scanner.nextInt();
-        numWifi = scanner.nextInt();
+        int numHouse = scanner.nextInt();
+        int numWifi = scanner.nextInt();
 
-        locationHouse = new int[numHouse];
-        for (int i = 0; i < numHouse; i++) {
+        int[] locationHouse = new int[numHouse];
+        for(int i=0; i<numHouse; i++) {
             locationHouse[i] = scanner.nextInt();
         }
         Arrays.sort(locationHouse);
-        System.out.print(findDistance() - 1);
+
+        int low = 1;
+        int high = locationHouse[numHouse - 1] - locationHouse[0] + 1;
+
+        while(high > low){
+            int mid = (high+low)/2;
+
+            int lastLocation = locationHouse[0];
+            int wifi = 1;
+            for(int i=1; i<numHouse; i++){
+                if((locationHouse[i] - lastLocation) >= mid){
+                    lastLocation = locationHouse[i];
+                    wifi++;
+                }
+            }
+
+            if(wifi < numWifi){
+                high = mid;
+            }else{
+                low = mid + 1;
+            }
+        }
+        System.out.print(low - 1);
     }
 }
