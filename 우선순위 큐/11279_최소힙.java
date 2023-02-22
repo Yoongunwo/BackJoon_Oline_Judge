ee74174a0932f6a3a8e99c712a16223699468410
Gunwo Yoon <97718735+Yoongunwo@users.noreply.github.com>
2023-02-22 15:54:29 +0900

    Create 11279_최소힙.java

---
+++ 우선순위 큐/11279_최소힙.java
@@ -0,0 +1,68 @@
+import java.util.Scanner;
+
+public class Main{ 
+    static int[] minHeap;
+    static void change(int i, int j){
+        int change = minHeap[i];
+        minHeap[i] = minHeap[j];
+        minHeap[j] = change;
+    }
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        int numCommand = scanner.nextInt();
+        minHeap = new int[100001];
+        int curNum = 0;
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0; i<numCommand; i++){
+            int command = scanner.nextInt();
+            if(command == 0){
+                if(curNum > 0) {
+                    sb.append(minHeap[1]).append("\n");
+                    minHeap[1] = minHeap[curNum];
+                    minHeap[curNum] = 0;
+                    curNum--;
+                    int compareIndex = 1;
+                    while(compareIndex * 2 <= curNum){
+                        if(compareIndex * 2 == curNum){
+                            if(minHeap[compareIndex] > minHeap[compareIndex*2]){
+                                change(compareIndex, compareIndex*2);
+                            }
+                            break;
+                        }else{
+                            if(minHeap[compareIndex * 2] < minHeap[compareIndex * 2 + 1]){
+                                if(minHeap[compareIndex * 2] < minHeap[compareIndex] ){
+                                    change(compareIndex * 2, compareIndex);
+                                    compareIndex *= 2;
+                                }else break;
+                            }else{
+                                if(minHeap[compareIndex * 2 + 1] < minHeap[compareIndex] ){
+                                    change(compareIndex * 2 + 1, compareIndex);
+                                    compareIndex = compareIndex * 2 + 1;
+                                }else break;
+                            }
+                        }
+                    }
+                }
+                else{
+                    sb.append("0\n");
+                }
+            }
+            else{
+                curNum++;
+                minHeap[curNum] = command;
+                int compareIndex = curNum;
+                while(compareIndex > 1){
+                    if(minHeap[compareIndex] < minHeap[compareIndex/2]){
+                        change(compareIndex, compareIndex/2);
+                        compareIndex /= 2;
+                    }else{
+                        break;
+                    }
+                }
+            }
+        }
+        System.out.print(sb);
+    }
+}
